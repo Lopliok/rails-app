38c16d23ef47c87529766f891ce81b470cea66b9
Lopliok <lopliok@gmail.com>
2017-12-22 12:45:01 +0100

    fdsf

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -8,4 +8,16 @@ class BuildingsController < ApplicationController
 
   end
 
+  def create
+    @building = Building.new(building_params)
+
+    @building.save
+    redirect_to @building
+  end
+
+  private
+  def building_params
+    params.require(:building).permit(:title, :code)
+  end
+
 end
